b7d33465f3712cdd06a63d467e6fb709c6d87c60
chyutronic <chyutronic@gmail.com>
2022-11-07 18:40:04 -0300

    Instalación de librería Tailwind ok

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
+import Encabezamiento from './componentes/compartidos/Encabezamiento';
+import Pie from './componentes/compartidos/Pie';
+import Principal from './componentes/compartidos/Principal';
 
 function App() {
   return (
     <div className="App">
+ 
+      <Encabezamiento></Encabezamiento>
+      <Principal></Principal>
+      <Pie></Pie>
 
     </div>
   );
---
+++ src/componentes/compartidos/Pie.js
@@ -0,0 +1,10 @@
+function Pie() {
+    return (
+        <footer className="text-red-500">
+            <small>Diseñado y Desarrollado por <strong>Christian Yutronic Villalobos</strong>. ©2022. Todos los derechos reservados.</small>
+        </footer>
+    );
+  }
+  
+  export default Pie;
+
