d3f1d18b62ddf0320c7d2e4cd62c5cb5eeb1e45a
chyutronic <chyutronic@gmail.com>
2022-11-09 17:40:42 -0300

    Se crea encabezado, aside y estructura 1ra Meta

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import './App.css';
 import Encabezamiento from './componentes/compartidos/Encabezamiento';
 import Pie from './componentes/compartidos/Pie';
 import Principal from './componentes/compartidos/Principal';
+import Meta from './componentes/lista/Meta';
 
 function App() {
   return (
     <div className="App">
  
       <Encabezamiento></Encabezamiento>
-      <Principal></Principal>
+      <Principal>
+        <Meta></Meta>
+      </Principal>
       <Pie></Pie>
 
     </div>
--- src/componentes/compartidos/Pie.js
+++ src/componentes/compartidos/Pie.js
@@ -1,6 +1,8 @@
+import './Pie.css';
+
 function Pie() {
     return (
-        <footer className="text-red-500">
+        <footer className="pie">
             <small>Diseñado y Desarrollado por <strong>Christian Yutronic Villalobos</strong>. ©2022. Todos los derechos reservados.</small>
         </footer>
     );
