4cb6f0a16c72616b6f2d3e99930b74ef3c751ecf
chyutronic <chyutronic@gmail.com>
2022-11-16 17:05:47 -0300

    Creación de nuevas metas y su vista en la lista

--- src/componentes/lista/Lista.js
+++ src/componentes/lista/Lista.js
@@ -1,42 +1,13 @@
+import { useContext } from "react";
 import Meta from "./Meta";
-
-const listaMock = [{
-    "id": "1",
-    "detalles": "Correr en 30 minutos",
-    "periodo": "día",
-    "eventos": 1,
-    "icono": "🏃",
-    "meta": 365,
-    "plazo": "2030-01-01",
-    "completado": 5
-},
-{
-    "id": "2",
-    "detalles": "Leer libros",
-    "periodo": "año",
-    "eventos": 6,
-    "icono": "📚",
-    "meta": 12,
-    "plazo": "2025-03-19",
-    "completado": 0
-},
-{
-    "id": "3",
-    "detalles": "Viajar a parques nacionales",
-    "periodo": "mes",
-    "eventos": 3,
-    "icono": "✈️",
-    "meta": 60,
-    "plazo": "2028-02-14",
-    "completado": 25
-}];
-
+import { Contexto } from "../../servicios/Memoria";
 
 function Lista() {
-    return (
-        listaMock.map(meta => <Meta {...meta}></Meta>)
-      );
+  const [estado, enviar] = useContext(Contexto);
+
+  return estado.orden.map((id) => (
+    <Meta key={id} {...estado.objetos[id]}></Meta>
+  ));
 }
 
 export default Lista;
-
